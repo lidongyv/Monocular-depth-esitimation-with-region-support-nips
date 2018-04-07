e4570f50ac5040b71270377d8fc0fe1f893b3ba8
pobingwanghai <pobingwanghai@gmail.com>
2018-04-07 19:49:14 +0800

    log

--- rsden/models/log.py
+++ rsden/models/log.py
@@ -2,9 +2,13 @@
 # @Author: yulidong
 # @Date:   2018-04-07 17:16:32
 # @Last Modified by:   yulidong
-# @Last Modified time: 2018-04-07 19:28:58
+# @Last Modified time: 2018-04-07 19:48:58
 4.7
 the baseline of rsden
 done:
 training baseline
 need:
+resolution problem
+now is the bilinear method to get the final estimation this is not sensibale
+we need to remove the deconvolutional layer in the network 
+or use the super resolution method
