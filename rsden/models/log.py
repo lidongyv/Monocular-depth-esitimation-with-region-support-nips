583322a9460777c90f3eb69cae80577007bd0dff
lidong <pobingwanghai@outlook.com>
2018-04-07 19:29:59 +0800

    log

---
+++ rsden/models/log.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: yulidong
+# @Date:   2018-04-07 17:16:32
+# @Last Modified by:   yulidong
+# @Last Modified time: 2018-04-07 19:28:58
+4.7
+the baseline of rsden
+done:
+training baseline
+need:
