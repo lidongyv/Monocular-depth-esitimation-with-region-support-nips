472f01cd1c37871adb2fc4f9eb692295f17029a8
lidong <pobingwanghai@outlook.com>
2018-04-07 14:57:39 +0800

    opt

--- reden/loader/nyu1topt.py
+++ reden/loader/nyu1topt.py
@@ -2,8 +2,7 @@
 # @Author: yulidong
 # @Date:   2018-04-05 19:13:11
 # @Last Modified by:   yulidong
-# @Last Modified time: 2018-04-05 19:13:30
-import scipy.io as sio    
+# @Last Modified time: 2018-04-06 15:41:46
 import matplotlib.pyplot as plt    
 import numpy as np
 import h5py    
@@ -18,12 +17,23 @@ for k, v in data.items():
 
 depths=data['depths']
 images=data['images']
-datas=[]
+testn=np.random.randint(0,2283,284)
+trains=[]
+tests=[]
 for i in range(2284):
+    print(i)
     image=np.array(images[i,:,:,:].astype('float32'))
     depth=np.array(depths[i,:,:])
     depth=np.reshape(depth,[1,depth.shape[0],depth.shape[1]])
     group=np.concatenate((image,depth),0)
     group=np.transpose(group,[0,2,1])
-    datas.append(group)
-np.save('/home/lidong/Documents/datasets/nyu/nyu_depth_data_labeled.npy',datas)
+    if (i in testn):
+        print('test')
+        tests.append(group)
+    else:
+        print('train')
+        trains.append(group)
+
+np.save('/home/lidong/Documents/datasets/nyu/trains.npy',trains)
+np.save('/home/lidong/Documents/datasets/nyu/tests.npy',tests)
+print('done')
---
+++ reden/loader/test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: yulidong
+# @Date:   2018-04-06 21:02:16
+# @Last Modified by:   yulidong
+# @Last Modified time: 2018-04-06 21:19:06
+import numpy as np
+print('fdas')
