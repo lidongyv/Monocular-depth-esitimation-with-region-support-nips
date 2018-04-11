2a2968ace3923dfe948ad7ebe87ff9aac4370536
pobingwanghai <pobingwanghai@gmail.com>
2018-04-11 13:24:57 +0800

    train_v2

--- rsden/models/__init__.py
+++ rsden/models/__init__.py
@@ -2,7 +2,7 @@
 # @Author: yulidong
 # @Date:   2018-03-18 15:24:33
 # @Last Modified by:   yulidong
-# @Last Modified time: 2018-04-07 15:11:34
+# @Last Modified time: 2018-04-10 21:29:38
 
 import torchvision.models as models
 
@@ -20,7 +20,7 @@ def _get_model_instance(name):
     try:
         return {
             'rsnet': rsn,
-
+            'rsnet_v2':rsn_v2,
         }[name]
     except:
         print('Model {} not available'.format(name))
--- rsden/models/log.py
+++ rsden/models/log.py
@@ -2,7 +2,7 @@
 # @Author: yulidong
 # @Date:   2018-04-07 17:16:32
 # @Last Modified by:   yulidong
-# @Last Modified time: 2018-04-07 19:48:58
+# @Last Modified time: 2018-04-10 21:01:37
 4.7
 the baseline of rsden
 done:
@@ -12,3 +12,6 @@ resolution problem
 now is the bilinear method to get the final estimation this is not sensibale
 we need to remove the deconvolutional layer in the network 
 or use the super resolution method
+4.10
+load resnet
+determine the depth space
