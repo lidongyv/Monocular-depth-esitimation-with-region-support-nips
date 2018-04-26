f7acb1e59f6ca023168aaf90c8550824b0eb70cf
lidong <pobingwanghai@163.com>
2018-04-26 22:26:36 +0800

    loss

--- region.py
+++ region.py
@@ -2,12 +2,12 @@
 # @Author: yulidong
 # @Date:   2018-04-26 10:28:48
 # @Last Modified by:   yulidong
-# @Last Modified time: 2018-04-26 18:43:56
+# @Last Modified time: 2018-04-26 20:51:26
 import os
 import numpy as np
 import cv2
 import copy
-def seg2regions(seg):
+def seg2instance(seg):
     region=seg
     label=-1
     while(np.max(seg)>0):
@@ -19,60 +19,84 @@ def seg2regions(seg):
             for i in range(1,positions.shape[1]):
                 if np.abs(positions[0,i]-positions[0,i-1])+np.abs(positions[1,i]-positions[1,i-1])<=2:
                      region[positions[0,i],positions[1,i]]=label
-                elif region[positions[0,i],positions[1,i]-1]==label or\
-                        region[positions[0,i]-1,positions[1,i]+1]==label or\
-                        region[positions[0,i]-1,positions[1,i]]==label or\
-                        region[positions[0,i]-1,positions[1,i]-1]==label:
+                elif region[positions[0,i],max(0,positions[1,i]-1)]==label or\
+                        region[max(0,positions[0,i]-1),min(positions[1,i]+1,639)]==label or\
+                        region[max(0,positions[0,i]-1),positions[1,i]]==label or\
+                        region[max(0,positions[0,i]-1),max(0,positions[1,i]-1)]==label:
                         region[positions[0,i],positions[1,i]]=label
                 else:
                     break;
         label=label-1
     return np.abs(region)
 
-def reg2supports(reg,dep,variance):
-    supports=np.zeros([reg.shape[0],reg.shape[1]])
+def reg2supportd(reg,dep,variance):
+    supportd=np.zeros([reg.shape[0],reg.shape[1]])
     for i in range(1,int(np.max(reg)+1)):
         region=np.where(reg==i)
         depth=dep[region]
         if(np.max(depth)-np.min(depth)<2*variance):
-            supports[region]=np.mean(depth)
+            supportd[region]=np.mean(depth)
 
         else:
             start=np.min(depth)
             end=np.min(depth)+variance
             while(end<np.max(depth)):
                 region1=np.where(np.logical_and(reg==i,np.logical_and(dep>start,dep<end)))
-                supports[region1]=np.mean(dep[region1])
-                if end+variance<np.max(depth):
-                    start=end
-                    end=end+variance
-                else:
-                    region1=np.where(np.logical_and(reg==i,np.logical_and(dep>start,dep<np.max(depth))))
-                    supports[region1]=np.mean(dep[region1])
-                    break;
-    return supports                    
+                if len(dep[region1])>len(depth)/((np.max(depth)-np.min(depth))/variance):
+                    supportd[region1]=np.mean(dep[region1])
 
-filenames=os.listdir('/home/lidong/Documents/datasets/nyu/tmp')
+                    if end+variance<np.max(depth):
+                        start=end
+                        end=end+variance
+                    else:
+                        region1=np.where(np.logical_and(reg==i,np.logical_and(dep>start,dep<np.max(depth))))
+                        supportd[region1]=np.mean(dep[region1])
+                        break;
+                else:
+
+                    if end+variance<np.max(depth):
+                        start=start
+                        end=end+variance
+                    else:
+                        region1=np.where(np.logical_and(reg==i,np.logical_and(dep>start,dep<np.max(depth))))
+                        supportd[region1]=np.mean(dep[region1])
+                        break;                    
+    return supportd                    
+load_path='/home/lidong/Documents/datasets/nyu/train'
+save_path='/home/lidong/Documents/datasets/nyu/train_region'
+filenames=os.listdir(load_path)
 for i in range(len(filenames)):
-    data=np.load(os.path.join('/home/lidong/Documents/datasets/nyu/tmp',filenames[i]))
+    data=np.load(os.path.join(load_path,filenames[i]))
+    if data.shape[2]>5:
+        np.save(os.path.join(load_path,filenames[i]),data[:,:,0:5])    
+    data=data[:,:,0:5]
     img = data[:,:,0:3]
     depth = data[:,:,3]
     segments = data[:,:,4]    
-    regions=seg2regions(copy.deepcopy(segments))
+    instance=seg2instance(copy.deepcopy(segments))
     variance=(np.max(depth)-np.min(depth))/10
-    supports=reg2supports(copy.deepcopy(regions),copy.deepcopy(depth),variance)
-    supportv=seg2regions(copy.deepcopy(supports))
-    # data=np.concatenate(data,np.reshape(regions,[regions.shape[0],regions.shape[1],1]),2)
-    # data=np.concatenate(data,np.reshape(supports,[supports.shape[0],supports.shape[1],1]),2)
-    # print(np.max(regions),np.max(regions),np.max(regions))
-    a=np.reshape(regions/np.max(regions),[supports.shape[0],supports.shape[1],1])
-    b=np.reshape(supports/np.max(supports),[supports.shape[0],supports.shape[1],1])
-    region_support=np.concatenate((a,b),2)
-    c=np.reshape(supportv/np.max(supportv),[supportv.shape[0],supportv.shape[1],1])
-    region_support=np.concatenate((region_support,c),2)
-    cv2.namedWindow('show')
-    cv2.imshow('show',region_support)
-    cv2.namedWindow('show2')
-    cv2.imshow('show2',b)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    supportd=reg2supportd(copy.deepcopy(instance),copy.deepcopy(depth),variance)
+    supporti=seg2instance(copy.deepcopy(supportd))
+    # data=np.concatenate((data,np.reshape(instance,[instance.shape[0],instance.shape[1],1])),2)
+    # data=np.concatenate((data,np.reshape(supportd,[supportd.shape[0],supportd.shape[1],1])),2)
+    # print(np.max(instance),np.max(instance),np.max(instance))
+    # a=np.reshape(supportd/np.max(supportd),[supportd.shape[0],supportd.shape[1],1])
+    # b=np.reshape(instance/np.max(instance),[supportd.shape[0],supportd.shape[1],1])
+    # c=np.reshape(supporti/np.max(supporti),[supporti.shape[0],supporti.shape[1],1])
+    # regionsupport_vision=np.concatenate((a,b),2)
+    # regionsupport_vision=np.concatenate((regionsupport_vision,c),2)
+    supportd=np.reshape(supportd,[supportd.shape[0],supportd.shape[1],1])
+    instance=np.reshape(instance,[instance.shape[0],instance.shape[1],1])
+    supporti=np.reshape(supporti,[supporti.shape[0],supporti.shape[1],1])
+    region_support=np.concatenate((supportd,instance),2)
+    region_support=np.concatenate((region_support,supporti),2)
+    data=np.concatenate((data,region_support),2)
+    print(os.path.join(save_path,filenames[i]))
+    np.save(os.path.join(save_path,filenames[i]),data)
+
+    # cv2.namedWindow('show')
+    # cv2.imshow('show',regionsupport_vision)
+    # # cv2.namedWindow('show2')
+    # # cv2.imshow('show2',b)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
--- rsden/loss.py
+++ rsden/loss.py
@@ -2,7 +2,7 @@
 # @Author: lidong
 # @Date:   2018-03-18 16:31:14
 # @Last Modified by:   yulidong
-# @Last Modified time: 2018-04-21 13:37:16
+# @Last Modified time: 2018-04-26 21:18:24
 
 import torch
 import numpy as np
@@ -40,4 +40,17 @@ def l1(input, target, weight=None, size_average=True):
     #print("pre_depth:%.4f,ground_depth:%.4f"%(torch.mean(input[1]).data.cpu().numpy().astype('float32'),torch.mean(target).data.cpu().numpy().astype('float32')))
     #output=relation+0.2*mean
     return relation
+
+def region(pre,supportd,supporti):
+    loss=torch.zeros(1)
+    for i in range(1,torch.max(supporti)):
+        pre_region=torch.where(supporti==i,pre,torch.zeros_like(pre))
+        ground_region=torch.where(supporti==i,supportd,torch.zeros_like(pre))
+        num=torch.sum(torch.where(supporti==i,torch.ones_like(pre),torch.zeros_like(pre)))
+        loss+=0.3*torch.abs(pre_region-ground_region)/num
+        average=torch.sum(pre_region)/num
+        variance=torch.sum(torch.where(pre_region>0,torch.square(pre_region-average),0))/num
+        loss+=0.7*variance
+
+
 
